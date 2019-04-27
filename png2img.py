345863363e703652422a3ed50a34cc254ec5dc96
y-ito <kfa00225@gmail.com>
2019-04-27 17:56:24 +0900

    add output 8bpp

--- png2img.py
+++ png2img.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from PIL import Image
+import argparse
 import sys
 
 # RGB値を16bit(RGB:565)のテキストに変換する
@@ -9,12 +10,26 @@ def rgb2hexstr(rgb):
     col = ((rgb[0]>>3)<<11) | ((rgb[1]>>2)<<5) | (rgb[2]>>3)
     return "0x{:04X}".format(col)
 
-def main():
-    if len(sys.argv) != 2:
-        print('Usage:png2img.py <pngfile>')
-        return
+# RGB値を8bit(RGB:332)のテキストに変換する
+def rgb8bithexstr(rgb):
+    col = ((rgb[0]>>5)<<5) | ((rgb[1]>>5)<<2) | (rgb[2]>>6)
+    return "0x{:02X}".format(col)
 
-    image = Image.open(sys.argv[1])
+def main():
+    # オプション解析
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument('pngfile', help='A filename of png image')
+    argparser.add_argument('-8', '--bpp8', action='store_true', help='Output with 8bpp hex text')
+    args = argparser.parse_args()
+#    print(args.pngfile)
+
+    # 出力形式を選択
+    outstr = rgb2hexstr
+    if args.bpp8 == True:
+        outstr = rgb8bithexstr
+
+    image = Image.open(args.pngfile)
     width, height = image.size
 
     print("// w,h:", width, height)
@@ -30,7 +45,8 @@ def main():
     for y in range(height):
         for x in range(width):
             pixel = getPixel(x, y)
-            print(rgb2hexstr(pixel) + ",", end="")
+            # print(rgb2hexstr(pixel) + ",", end="")
+            print(outstr(pixel) + ",", end="")
             x_cnt = x_cnt + 1
             if x_cnt >= 8:
                 x_cnt = 0
